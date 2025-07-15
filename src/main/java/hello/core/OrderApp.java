5e1453223ebcde305db0a8e0bedca78230664c70
jngbn <[user email]>
2025-07-15 12:48:19 +0900

    주문과 할인 도메인 실행과 테스트

---
+++ src/main/java/hello/core/OrderApp.java
@@ -0,0 +1,21 @@
+package hello.core;
+
+import hello.core.Order.Order;
+import hello.core.Order.OrderServiceImpl;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberServiceImpl;
+
+public class OrderApp {
+    public static void main(String[] args) {
+        MemberServiceImpl memberService = new MemberServiceImpl();
+        OrderServiceImpl orderService = new OrderServiceImpl();
+
+        long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        System.out.println("order = " + order);
+    }
+}
