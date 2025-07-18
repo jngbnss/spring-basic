73ea30c8eae79138e697581d2069aa6120f85431
jngbn <[user email]>
2025-07-18 13:06:57 +0900

    웹 애플리케이션과 싱글톤

--- src/main/java/hello/core/Order/OrderServiceImpl.java
+++ src/main/java/hello/core/Order/OrderServiceImpl.java
@@ -8,10 +8,8 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
-    private final MemberRepository memberRepository; // = new MemoryMemberRepository();
-//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-    private final DiscountPolicy discountPolicy; // = new RateDiscountPolicy();
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
